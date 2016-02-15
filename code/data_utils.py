50d0d3a6963b5f83d9605ac0ef8d5ae2f00cd99c
Lisa Wang <lisa1010@stanford.edu>
2016-02-14 16:52:40 -0800

    added shellscript to download cifar and edited mnist shellscript

---
+++ code/data_utils.py
@@ -0,0 +1,19 @@
+# data_utils.py
+# @author: Lisa Wang
+# @created: Feb 14 2016
+#
+#===============================================================================
+# DESCRIPTION:
+#
+# A collection of functions that return train, val and test data for
+# various datasets
+#
+#===============================================================================
+# CURRENT STATUS: In progress
+#===============================================================================
+# USAGE: from data_utils import *
+# 
+
+
+def load_cifar10():
+
