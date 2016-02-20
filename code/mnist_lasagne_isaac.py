cbc0db9017987cccdb68cf57b6571675563253ae
icaswell <icaswell@stanford.edu>
2016-02-20 10:40:22 -0800

    archived keras files

---
+++ code/mnist_lasagne_isaac.py
@@ -0,0 +1,24 @@
+import os, struct
+from array import array as pyarray
+from numpy import append, array, int8, uint8, zeros
+import numpy as np
+from data_utils import load_mnist, load_mnist_fail
+from loopy_network_lasagne import LoopyNetwork
+import util 
+
+# model = LoopyNetwork(architecture_fpath="../architectures/mnist_c3_c5_sm.py", n_unrolls=1, batch_size=36)
+model = LoopyNetwork(architecture_fpath="../architectures/mnist_c3_c3_c1_fc+loop.py", n_unrolls=3, batch_size=36)
+print repr(model)
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_mnist()
+
+print X_train.shape, y_train.shape
+print X_test.shape, y_test.shape
+
+check_error_n_batches = 300
+history = model.train_model(X_train, y_train, X_test, y_test, n_epochs=20, use_expensive_stats=True, check_error_n_batches=check_error_n_batches)
+print history
+
+
+
+util.plot_loss_acc(history["full_train_loss"], history["full_train_acc"], history["valid_acc"], "batches*%s"%check_error_n_batches, attributes={"isaac": np.random.rand()})
--- code/mnist_nonloopy.py
+++ code/mnist_nonloopy.py
@@ -6,23 +6,14 @@ from data_utils import load_mnist
 import util
 from loopy_network_lasagne import LoopyNetwork
 
-model = LoopyNetwork(architecture_fpath="../architectures/mnist_nonloopy_config.py", n_unrolls=1, batch_size=50)
+model = LoopyNetwork(architecture_fpath="../architectures/mnist_nonloopy_config.py", n_unrolls=1, batch_size=36)
 print repr(model)
 
 X_train, y_train, X_val, y_val, X_test, y_test = load_mnist()
 
-print 'X_train shape: {}, y_train shape: {}'.format(X_train.shape, y_train.shape)
-print 'X_val shape: {}, y_val shape: {}'.format(X_val.shape, y_val.shape)
-print 'X_test shape: {}, y_test shape: {}'.format(X_test.shape, y_test.shape)
-num_test_samples = X_test.shape[0]
+print X_train.shape, y_train.shape
+print X_test.shape, y_test.shape
 
-model.train_model(X_train, y_train, X_val, y_val, use_expensive_stats=True, n_epochs=10)
-
-util.plot_loss_acc(history["full_train_loss"], history["full_train_acc"], history["valid_acc"], "batches*%s"%check_error_n_batches, attributes={"lol": 3})
-
-# evaluate on test set
-print 'Testing on a set of {} samples'.format(num_test_samples)
-test_loss, test_acc = model.performance_on_whole_set(X_test, y_test)
-
-print 'Test loss: {}, test accuracy: {}'.format(test_loss, test_acc)
-
+history = model.train_model(X_train, y_train, X_test, y_test, use_expensive_stats=True, n_epochs=50)
+	
+util.plot_loss_acc(history["full_train_loss"], history["full_train_acc"], history["valid_acc"], "batches*%s"%check_error_n_batches, attributes={"augh": 3})
