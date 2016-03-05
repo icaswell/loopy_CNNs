76ede0bb70c134c42b1cc7804aec24cac955975f
icaswell <icaswell@stanford.edu>
2016-03-04 18:04:58 -0800

    added my testing stuff

---
+++ architectures/cifar_isaac.py
@@ -0,0 +1,73 @@
+# File: toy_loopy_mlp_lasagne_config.py
+# @author: Isaac Caswell
+# @created: Feb 7 2016
+# 
+
+{
+"run_params":
+	{"use_batchnorm":True},
+"framework":"lasagne",
+"name": "cifar_c3-64_c3-64_c3-128_c3-128_c3-3_fc",
+
+"templates": 
+	{
+	"input": {"type": "input"},
+	"conv3": {"type": "conv2d", "W": "glorot_uniform", "filter_size": 3, "pad": 1},
+	"conv5": {"type": "conv2d", "W": "glorot_uniform", "filter_size": 5, "pad": 2},	
+	"pool": {"type": "pool2d", "pool_size":2},
+	"dense": {"type": "dense", "W": "glorot_uniform"},
+	},
+
+#===============================================================================
+# Here's where you define your layers.  You'll wire them together later.
+
+"layers": #parser asserts that there exists a layer called "input" and a layer called "output"
+	{
+	"input":{"output_dim":(3,32,32), "template": "input"},
+	"conv_1":{"num_filters":64, "template": "conv3"},
+	"conv_2":{"num_filters":64, "template": "conv3"},
+	"conv_3":{"num_filters":128, "template": "conv3"},
+	"conv_4":{"num_filters":128, "template": "conv3"},	
+	"conv_5":{"num_filters":3, "template": "conv3"},
+	# "layer_2":{"num_filters":8, "template": "conv"},            
+	# "layer_2_pool":{"template": "pool"},
+	"fc_1":{"output_dim":10, "template": "dense", "nonlinearity":"softmax"},
+	},
+
+"stacks":
+	{
+	"main_stack": {
+		"type": "main",
+		"structure": ["input", "conv_1", "conv_2", "conv_3", "conv_4", "conv_5", "fc_1"]
+		},
+	# "loop": {
+	# 	"type": "loop",
+	# 	"structure": ["conv_3", "conv_1"],
+	# 	"composition_mode": "sum"
+	# 	},		
+			
+	},
+
+"layer_defaults":
+	{
+	"dense": 
+		{		
+		"W": "glorot_uniform",
+		"nonlinearity": "relu"
+		},	
+	"conv2d": 
+		{		
+		"W": "glorot_uniform",
+		"num_filters": 3,
+		"filter_size": 3,
+		"pad": 1,
+		"nonlinearity": "relu"
+		},
+
+		# class lasagne.layers.MaxPool2DLayer(incoming, pool_size, stride=None, pad=(0, 0), ignore_border=True, **kwargs)[source]	
+	"pool2d":
+		{
+		}
+	}
+}
+
